e2c683f3483ffa9fc2dbf57aff1eec27d8e6f589
blha303 <stevensmith.ome@gmail.com>
2015-06-13 15:43:23 +0800

    Initial

---
+++ hypemeter.py
@@ -0,0 +1,19 @@
+from tornado import gen
+from tornado.websocket import websocket_connect
+from tornado.ioloop import IOLoop, PeriodicCallback
+from singlemessagebot import SingleMessageBot
+from json import loads
+from sys import argv
+import requests
+
+hype = 2
+
+def specifyHype():
+    global hype
+    hype = (hype + 1) * 3
+    bot = SingleMessageBot("Hype level is now {}".format(hype))
+    bot.start()
+
+callback = PeriodicCallback(specifyHype, 86400000) # daily
+callback.start()
+IOLoop.instance().start()
